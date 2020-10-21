af7bbf8f2336e324bb0d5bea9f4046abe1294f46
Jon Robison <narfman0@gmail.com>
2020-10-21 11:12:15 -0400

    -Search multiple build,job pairs

--- buildkite_log_parse/main.py
+++ buildkite_log_parse/main.py
@@ -5,17 +5,17 @@ from buildkite_log_parse.parser import Parser
 
 
 def get_build_and_job(job_name, builds_response, build_message):
-    """ Parse pipelines for active build and extract string """
+    """ Parse builds response for job """
     for build in builds_response:
         if re.match(build_message, build["message"]):
             for job in build["jobs"]:
                 if job["name"] == job_name:
-                    return build, job
+                    yield build, job
     return None
 
 
 def extract_job_string(job_log, regex, group=None):
-    """ Extract the ssh string to connect to from log """
+    """ Extract a string matching the regex from the log """
     matcher = re.compile(regex, re.MULTILINE)
     match = matcher.search(job_log)
     if group is not None:
@@ -32,18 +32,22 @@ def main():
         parser.token(),
         parser.debug(),
     )
-    build, job = get_build_and_job(
+    for build, job in get_build_and_job(
         parser.job(), builds_response, parser.build_message(),
-    )
-    log = build_job_log(
-        build,
-        job,
-        parser.organization(),
-        parser.pipeline(),
-        parser.token(),
-        parser.debug(),
-    )
-    print(extract_job_string(log, parser.regex(), parser.group()))
+    ):
+        log = build_job_log(
+            build,
+            job,
+            parser.organization(),
+            parser.pipeline(),
+            parser.token(),
+            parser.debug(),
+        )
+        try:
+            print(extract_job_string(log, parser.regex(), parser.group()))
+        except:
+            # fall back on the next matching build.job pair
+            continue
 
 
 if __name__ == "__main__":
